87fbbe9216a4834ad1f746a4e8c5b688fd1800d0
SenRaice <sen.rai@outlook.com>
2022-11-19 19:14:51 +0000

    Questions
    hello

---
+++ src/Question.java
@@ -0,0 +1,11 @@
+package hackathon;
+
+public class Booleanthing {
+	public boolean a;
+	public String q; 
+	public Booleanthing(boolean b, String r) 
+	{
+		a=b;
+		q=r;
+	}
+}
---
+++ src/Questions.java
@@ -0,0 +1,55 @@
+package hackathon;
+import swiftbot.SwiftBotAPI;
+
+public class Hackathon1 {
+	
+	
+	
+
+public static void Easy_Questions() {
+	
+	Booleanthing questions [] = 
+		{
+		    new Booleanthing(false, "The chairs in the House of Common are red."),
+			new Booleanthing(true,"The Eiffel Tower is located in Paris. A:True"),
+			new Booleanthing(false, "1+1=5. A:False"),
+			new Booleanthing(true,"WW1 began in the year 1914."),
+			new Booleanthing(true,"Mount Everest is the tallest mountain in the world."),
+			new Booleanthing(false, "Romeo and Juliet was written by JK Rowlings."),
+			new Booleanthing(false,"January has 30 days."),
+			new Booleanthing(true, "Ten Thousand has 4 zeros."),
+			new Booleanthing(false, "Coldplay are a dance group."),
+			new Booleanthing(true,"Light is faster than Sound."),
+		};
+	
+}
+public static void Medium_Questions() {
+	Booleanthing questions[]= {
+			new Booleanthing(false,"Harry Potter was almost sorted into HufflePuff."),
+			new Booleanthing(true, "A touchdown is worth 6 points."),
+			new Booleanthing(false,"Oxygen is the most common in the Atmosphere."),
+			new Booleanthing(false, "Mike Tyson is known as The Greatest of All Time."),
+			new Booleanthing(true, "The very first model of the iphone was released in 2007."),
+			new Booleanthing(false, "Washington DC is the most populated city in USA."),
+			new Booleanthing(false,"Elephants lay the largest egg"),
+			new Booleanthing(true,"Mars is also known as The Red Planet"),
+			new Booleanthing(false,"Bucharest isn't the Capital City of Romania"),
+			new Booleanthing(true,"A baby kangaroo is called Joey")
+};
+}
+public static void Hard_Questions(boolean a, String b){
+	Booleanthing questions[]= {
+			new Booleanthing(false, "The Fibulas is the longest bone in the body"),
+			new Booleanthing(false, "Pluto is the smallest planet in the Solar System"),
+			new Booleanthing(true, "The heart has 4 valves"),
+			new Booleanthing(false, "Octopuses have 3 hearts"),
+			new Booleanthing(false, "Plutonium has the chemical symbol Pb"),
+			new Booleanthing(true, "Heptadecagon has 17 sides"),
+			new Booleanthing(false, "Goa is the colony of Italy"),
+			new Booleanthing(true,"Tamil is the oldest language in the world"),
+			new Booleanthing(true,"30 Countries fought in the Second World War"),
+			new Booleanthing(false,"Qatar has the oldest flag in the world"),
+			
+	};
+}
+}
