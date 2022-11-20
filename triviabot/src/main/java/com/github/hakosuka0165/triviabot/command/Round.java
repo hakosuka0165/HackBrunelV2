366e6978a59107a0373874cff8b708ef582f3530
hakosuka0165 <113927577+hakosuka0165@users.noreply.github.com>
2022-11-20 01:00:30 +0000

    Round.java: Button Response Tracking
    The bot can now keep track of events occurring as a result of responses

--- triviabot/src/main/java/com/github/hakosuka0165/triviabot/command/Round.java
+++ triviabot/src/main/java/com/github/hakosuka0165/triviabot/command/Round.java
@@ -1,13 +1,15 @@
 package com.github.hakosuka0165.triviabot.command;
 
 import org.javacord.api.entity.message.MessageBuilder;
+import org.javacord.api.entity.message.MessageDecoration;
 import org.javacord.api.entity.message.component.ActionRow;
 import org.javacord.api.entity.message.component.Button;
 import org.javacord.api.event.message.MessageCreateEvent;
+import org.javacord.api.interaction.MessageComponentInteraction;
 import org.javacord.api.listener.message.MessageCreateListener;
 
-import com.github.hakosuka0165.triviabot.Main;
 import com.github.hakosuka0165.triviabot.Bibliotheque;
+import com.github.hakosuka0165.triviabot.Main;
 import com.github.hakosuka0165.triviabot.Question;
 
 public class Round implements MessageCreateListener {
@@ -19,6 +21,10 @@ public class Round implements MessageCreateListener {
 			Question[] container = Bibliotheque.library;
 			Question[] shuffledContainer = Bibliotheque.ShuffleQuestions(container);
 			
+			new MessageBuilder()
+			.append("Beginning round!", MessageDecoration.BOLD)
+			.send(event.getChannel());
+			
 			for (int i = shuffledContainer.length - 1; i > 0; --i) {
 				new MessageBuilder()
 				.setContent(shuffledContainer[i].q)
@@ -26,6 +32,24 @@ public class Round implements MessageCreateListener {
 						ActionRow.of(Button.success("success", "True"),
 								Button.danger("danger", "False")))
 				.send(event.getChannel());
+				
+				event.getApi().addMessageComponentCreateListener(e -> {
+					MessageComponentInteraction messageComponentInteraction = e.getMessageComponentInteraction();
+					String customId = messageComponentInteraction.getCustomId();
+					
+					switch(customId) {
+					case "success":
+						messageComponentInteraction.createImmediateResponder()
+						.setContent("Answer recorded for " + messageComponentInteraction.getUser() + "(" + customId + ")")
+						.respond();
+					break;
+					case "danger":
+						messageComponentInteraction.createImmediateResponder()
+						.setContent("Answer recorded for " + messageComponentInteraction.getUser() + "(" + customId + ")")
+						.respond();
+					break;
+					}
+				});
 			}
 		}
 	}
