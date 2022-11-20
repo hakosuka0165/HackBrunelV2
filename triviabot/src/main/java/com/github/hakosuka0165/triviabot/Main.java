1bdf25e54eb2954d4d7123db5c10d42eb152dfce
Slender2000 <64808609+Slender2000@users.noreply.github.com>
2022-11-20 11:17:40 +0000

    Revert "Delete triviabot directory"
    This reverts commit eea44c88f43a7d2e37dcedc3aa2882c24bfd48e9.

---
+++ triviabot/src/main/java/com/github/hakosuka0165/triviabot/Main.java
@@ -0,0 +1,39 @@
+package com.github.hakosuka0165.triviabot;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.javacord.api.DiscordApi;
+import org.javacord.api.DiscordApiBuilder;
+import org.javacord.api.entity.activity.Activity;
+import org.javacord.api.entity.channel.Channel;
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.intent.Intent;
+import org.javacord.api.entity.message.MessageBuilder;
+import org.javacord.api.entity.message.component.ActionRow;
+import org.javacord.api.entity.message.component.Button;
+
+import com.github.hakosuka0165.triviabot.command.Round;
+
+public class Main {
+	public static String prefix = "!";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String token = "MTA0MzUxMDU5NjE2MDcyOTE4MQ.GndWB4.BhgHGzg10agkQU3q9YhuSCRdzbb4FTfHocKDU4";
+		
+		DiscordApi api = new DiscordApiBuilder()
+				.setToken(token)
+				.addIntents(Intent.MESSAGE_CONTENT)
+				.login().join();
+		
+		api.addMessageCreateListener(new Round());
+		
+		api.addMessageComponentCreateListener(event -> {
+			event.getMessageComponentInteraction().acknowledge();
+			System.out.println("Hello!");
+		});
+		
+		System.out.println("You can invite the bot using the following URL\n" + api.createBotInvite());
+	}
+}
